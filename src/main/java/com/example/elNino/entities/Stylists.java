0f610f4ba0e31a7db638bc2cecc5af77d9524067
tiegoouedraogo <tiego.ouedraogo@peopleshores.com>
2024-09-10 16:50:48 -0400

    constructors and setter created

--- src/main/java/com/example/elNino/entities/Stylists.java
+++ src/main/java/com/example/elNino/entities/Stylists.java
@@ -30,4 +30,63 @@ public class Stylists {
     @JoinColumn(name = "customer_id", nullable = false)
     private List<Customer> customer_id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public Long getStylist_id() {
+        return stylist_id;
+    }
+
+    public String getStylist_name() {
+        return stylist_name;
+    }
+
+    public String getSpecilizations() {
+        return specilizations;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public List<Customer> getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setStylist_id(Long stylist_id) {
+        this.stylist_id = stylist_id;
+    }
+
+    public void setStylist_name(String stylist_name) {
+        this.stylist_name = stylist_name;
+    }
+
+    public void setSpecilizations(String specilizations) {
+        this.specilizations = specilizations;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    public void setCustomer_id(List<Customer> customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public Stylists(Long id, Long stylist_id, String stylist_name, String specilizations, String availability, List<Customer> customer_id) {
+        this.id = id;
+        this.stylist_id = stylist_id;
+        this.stylist_name = stylist_name;
+        this.specilizations = specilizations;
+        this.availability = availability;
+        this.customer_id = customer_id;
+    }
+
+    public Stylists() {}
+
 }
