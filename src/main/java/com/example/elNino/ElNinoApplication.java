2991a47179b5e95f3b2b62343fb7020056d39b6d
Gillesedgy <edgygilles@pursuit.org>
2024-09-10 15:01:47 -0400

    initial set up

---
+++ src/main/java/com/example/elNino/ElNinoApplication.java
@@ -0,0 +1,13 @@
+package com.example.elNino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElNinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElNinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/elNino/ElNinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elNino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElNinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
